b0eb0d62bb3c149cca5bd1b606225dd702a70389
Kyle DeGuzman <48830018+KyleADeGuzman@users.noreply.github.com>
2020-02-11 08:12:42 -0500

    Update Movie.java

--- Unit5Labs/src/Movie.java
+++ Unit5Labs/src/Movie.java
@@ -4,30 +4,32 @@ public class Movie {
 	 private String studioofmov;
 	 private String ratingofmov;
 	 
+	//constructor for the class movie 
 	 public Movie(String t, String s, String r) {
 		 titleofmov = t;
 		 studioofmov = s;
 		 ratingofmov = r;
 		}
 	 
+	//Constructor for the class movie
 	 public Movie (String t, String s) {
-		  titleofmov = t;
+		 titleofmov = t;
 		 studioofmov = s;
 		 ratingofmov = "PG";
 		}
-	 
+	 //method for getpg
 	 public static Movie[] getPG(Movie[] mov) {
 		 Movie[] pgMovie = new Movie[mov.length];
-		 int newArrayIndex = 0;
+		 int Array = 0;
 		 for (int i = 0; i < mov.length; i++) {
-		 if (mov[i].rating.equals("PG")) {
-		 pgMovie[newArrayIndex] = mov[i];
-		 newArrayIndex++;
-		 }
+		 	if (mov[i].rating.equals("PG")) {
+		 		pgMovie[Array] = mov[i];
+		 		Array++;
+		 		}
 		 }
 		 return pgMovie;
 		}
-	 
+	 //intance of the class movie
 	 Movie mov = new Movie("Casino Royale","Eon Productions","PG-13"); 
 		
 }
